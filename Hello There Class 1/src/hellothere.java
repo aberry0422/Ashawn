c88de1359ffc57d79f631816916af3d5c55d1444
aberry0422 <aberry0422@10.242.0.187>
2019-09-05 12:17:26 -0400

    Project Done

---
+++ Hello There Class 1/src/hellothere.java
@@ -0,0 +1,26 @@
+
+public class hellothere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Project Name: 10 things I want to do before I die
+		
+	System.out.println("1. I want to become very sucessful in the future.");
+	System.out.println("2. I want to get on a plane");
+	System.out.println("3. I want to go to a different country ");
+	System.out.println("4. I want to go to Greece");
+	System.out.println("5. I want to drive an ATV");
+	System.out.println("6. I want to have my own comapany");
+	System.out.println("7. I want to learn how to play the piano");
+	System.out.println("8. I want to drive a Maserati");
+	System.out.println("9. I want to go to Hawaii");
+	System.out.println("10. I want to buy a house in California");
+	
+	
+	System.out.print("11. This line one.\n\tThis is line two");
+	
+
+	}
+
+
+}
