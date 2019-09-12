ca479a7eb7c89110516ce2a21c5e8158af65a09f
aberry0422 <aberry0422@10.242.0.187>
2019-09-11 20:18:47 -0400

    This is my homework

---
+++ Hello There Class 1/src/program1.java
@@ -0,0 +1,21 @@
+
+public class program1 {
+	//Project Name: Program 1
+	//Name: Ashawn C Berry
+	//Last Modified: 9/10/19
+	//Version: 1.0
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Ouput core class info
+		
+		int age = 18; 
+		String myNumber = "35"      ;
+		String middleInital = "C"; 
+		String myName = "Ashawn";
+		System.out.println("Hello " + myName + "!\nYour favor " + myNumber + ".");
+		System.out.print("I'm " + age +" My middle intial is " + middleInital );	
+
+	}
+
+}
