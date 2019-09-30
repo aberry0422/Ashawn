70cc5528d242abffffb9ce37052939201046111c
aberry0422 <aberry0422@10.242.0.187>
2019-09-30 10:27:01 -0400

    This is my homework

---
+++ Hello There Class 1/src/AshawnBerryProgram2.java
@@ -0,0 +1,31 @@
+//Import required code
+import java.util.Scanner;
+//Start Keyboard SCaanner
+
+
+
+// Project Name: Program 2
+//Name: Ashawn C Berry
+//Github:aberry0422
+//Last Modified: 9/19/19
+//Version: 1.0
+public class AshawnBerryProgram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Declare variables
+		int first = 0;
+		int secound = 0;
+		int third = 0;
+		Scanner keyboard = new Scanner("System.in");
+		System.out.println("What is your first number?");
+		first = keyboard.nextInt();
+		System.out.println("What is your secound number?");
+		secound = keyboard.nextInt();
+		System.out.println("What is your third number?");
+		third = keyboard.nextInt();
+	
+	}
+
+}
