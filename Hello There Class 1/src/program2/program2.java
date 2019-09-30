10ebaf3ebe9600e9f8b36a5be0a2530e21a24320
aberry0422 <aberry0422@10.242.0.187>
2019-09-30 10:26:35 -0400

    This is my homework

--- Hello There Class 1/src/program2/program2.java
+++ Hello There Class 1/src/program2/program2.java
@@ -1,5 +1,9 @@
 /**
  */
+//Project Name: Program 2
+//Author: Ashawn Berry
+//version: 1.0
+//Last modified: 9/23/19
 package program2;
 
 
@@ -10,11 +14,17 @@ public class program2 {
 	
 	public static void main(String[] args) {
 	Scanner keyboard = new Scanner(System.in);
+	//Dont forget to turn the keyboard on and turn it off
 		// TODO Auto-generated method stub
+	// The outcome should allow you to select any number
+	// The outcome should also be like using a calculator.
+	
 int first = 0;
 int secound = 0;
 int third = 0; 
-
+//The variable determines the outcome fore your answers
+System.out.println("Main Menu");
+System.out.println("================");
 System.out.println("What is your first number?");
 first = keyboard.nextInt();
 System.out.println("What is your secound number?");
@@ -22,15 +32,18 @@ secound = keyboard.nextInt();
 System.out.println("What is your third number?");
 third = keyboard.nextInt();
 
+
 System.out.println("1.) Add");
 System.out.println("2.) Subtract");
 System.out.println("3.) Multiply");
 System.out.println("4.) Divide");
+// These will be your menu options that you can choose from
 int choice = keyboard.nextInt();
 double ans;
 
 switch(choice) {
 case 1: 
+	//addition formula
 	ans= first + secound;
 	System.out.println(first + "+" + secound + "=" + ans);
 	if (ans > third) {
@@ -44,6 +57,7 @@ case 1:
 	}
 	break;
 case 2: 
+	//subtraction formula
 	ans= first - secound;
 	System.out.println(first + "-" + secound + "=" + ans);
 	if (ans > third) {
@@ -57,6 +71,7 @@ case 2:
 	}
 	break;
 case 3: 
+	//multiplication formula
 	ans= first * secound;
 	System.out.println(first + "*" + secound + "=" + ans);
 	if (ans > third) {
@@ -70,6 +85,7 @@ case 3:
 	}
 	break;
 case 4: 
+	// Division formula
 	ans= first / secound;
 	System.out.println(first + "/" + secound + "=" + ans);
 	if (ans > third) {
@@ -83,9 +99,27 @@ case 4:
 	
 	}
 	break;
+	default:
+			System.out.println("Thank you.  Good bye.");
+			
+			//close keyboard
+			keyboard.close();
 	
 }
 	}
+	//Security static return name (Pass in)
+	//would be public static void if you were not sending 
 	
+	public static int addition(int first, int secound)
+	{
+		int ans = first + secound;	
+		return ans; 
+	}
+	
+	public static int subtraction(int first, int secound)
+	{
+		int ans = first - secound;
+		return ans; 
+	}
 
 }
